b6565abb5a53448a2f0e7a0f49b9c085234874c5
Enrique <enrique.fernandez34@educa.madrid.org>
2026-05-05 10:50:23 +0200

    reset

---
+++ src/prueba.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alumnotd
+ */
+public class prueba {
+	public static void main(String[] args)
+	{
+		AhorcadoJuego ahorcado = new AhorcadoJuego();
+		System.out.println(ahorcado.buscar('A'));
+
+	}
+}
